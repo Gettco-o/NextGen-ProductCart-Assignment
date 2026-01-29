06d9ceb3b9a4bcbdf01beace400859755ec618df
Gettco-o <ecovenant74@gmail.com>
2026-01-29 23:17:02 +0100

    added notfound c

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { AllProducts } from './all-products/all-products';
 import { SingleProduct } from './single-product/single-product';
 import { Cart } from './cart/cart';
+import { Notfound } from './notfound/notfound';
 
 export const routes: Routes = [
       {path: '', component: AllProducts},
       {path: 'products/:id', component: SingleProduct},
-      {path: 'cart', component: Cart}
+      {path: 'cart', component: Cart},
+      {path: '**', component: Notfound}
 ];
 
