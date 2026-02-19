6fe8357720f49e2bb553459caefa02d58ac9e69a
Gettco-o <ecovenant74@gmail.com>
2026-02-19 10:28:48 +0100

    lazy loaded routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { PreloadAllModules, provideRouter, withPreloading } from '@angular/router';
 
 import { routes } from './app.routes';
 
@@ -12,7 +12,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes),
+    provideRouter(routes, withPreloading(PreloadAllModules)),
     provideHttpClient(withInterceptors([authInterceptor]))
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,12 @@
 import { Routes } from '@angular/router';
-import { AllProducts } from './all-products/all-products';
-import { SingleProduct } from './single-product/single-product';
-import { Cart } from './cart/cart';
-import { Notfound } from './notfound/notfound';
-import { NewProduct } from './new-product/new-product';
-import { Login } from './login/login';
 import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
-      {path: '', component: Login},
-      {path: 'products', component: AllProducts, canActivate: [authGuard]},
-      {path: 'products/:id', component: SingleProduct, canActivate: [authGuard]},
-      {path: 'cart', component: Cart, canActivate: [authGuard]},
-      {path: 'product/new', component: NewProduct, canActivate: [authGuard]},
-      {path: '**', component: Notfound}
+      {path: '', loadComponent: () => import('./login/login').then(m => m.Login)},
+      {path: 'products', loadComponent: () => import('./all-products/all-products').then(m => m.AllProducts), canActivate: [authGuard]},
+      {path: 'products/:id', loadComponent: () => import('./single-product/single-product').then(m => m.SingleProduct), canActivate: [authGuard]},
+      {path: 'cart', loadComponent: () => import('./cart/cart').then(m => m.Cart), canActivate: [authGuard]},
+      {path: 'product/new', loadComponent: () => import('./new-product/new-product').then(m => m.NewProduct), canActivate: [authGuard]},
+      {path: '**', loadComponent: () => import('./notfound/notfound').then(m => m.Notfound)}
 ];
 
